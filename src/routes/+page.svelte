5217e54858dfb9915ac058cf669b2eb215296f92
Raymond <raymond.goo124@gmail.com>
2023-02-01 14:48:21 +0800

    package.json: Switch to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "./page.css"
+</script>
+<div class="page">
+    <img src="https://cloud-i7e0fez7u-hack-club-bot.vercel.app/0codeday-notext-white.png" height="90px" width="90px" alt="CodeDay logo">
+    <h1 class="text-4xl font-bold">We're building this page currently.</h1>
+    <p class="text-2xl font-bold">Check back soon for more updates!</p>
+</div>
